141fba3804a1309cefe9285d5640cff63c3a9dd2
Omar-Codess <omartaki8@gmail.com>
2023-03-16 16:39:02 +0100

    aggiunto metodo send()

--- src/org/lessons/java/christmas/ChristmasLetter.java
+++ src/org/lessons/java/christmas/ChristmasLetter.java
@@ -1,7 +1,11 @@
 package org.lessons.java.christmas;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class ChristmasLetter {
+
+
     String nome;
     String indirizzo;
     List<String> listaDesideri;
@@ -15,29 +19,46 @@ public class ChristmasLetter {
     public String getNome() {
         return nome;
     }
-
     public void setNome(String nome) {
         this.nome = nome;
     }
-
     public String getIndirizzo() {
         return indirizzo;
     }
-
     public void setIndirizzo(String indirizzo) {
         this.indirizzo = indirizzo;
     }
-
     public List<String> getListaDesideri() {
         return listaDesideri;
     }
-
     public void setListaDesideri(List<String> listaDesideri) {
         this.listaDesideri = listaDesideri;
     }
 
     //METODO SEND()
     public void send(){
+        if (listaDesideri.size() > 5){
+            try {
+                List<String> listaDesideri = new ArrayList<>();
+            } catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        } else {
+            System.out.println(nome);
+            System.out.println(indirizzo);
+            System.out.println(listaDesideri);
+        }
+
+        Random rdn = new Random();
+        boolean buonoCattivo = true;
+        boolean buonoCativo = rdn.nextBoolean();
+        if (!buonoCativo){
+            try {
+                List<String> listaDesideri = new ArrayList<>();
+            } catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
 
     }
 }
